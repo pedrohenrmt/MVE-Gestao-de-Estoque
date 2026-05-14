f2c6b11490234dc6160811984519ea5654298f7b
Pedro Henrique <phpedro5530@gmail.com>
2026-05-14 10:29:22 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 print("Sistema de Gestão de Estoque Iniciado!")
-<<<<<<< HEAD
-print("Ola mundo")
-print("HELLO WORD")
-=======
-
->>>>>>> 618aa56c18ef6f719a4128c17345925b0ddbfe85
+print("Ola word")
+print("Teste de commit")
