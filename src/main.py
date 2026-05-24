17be21446e6847d34817c0fdacaa11aba3e4b7a6
Pedro Henrique Lima <phpedro5530@gmail.com>
2026-05-24 15:32:07 -0300

    Criação inicial da interface

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("Ola mundo")
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title("Sistema de Estoque")
+
+janela.geometry("500x300")
+
+titulo = tk.Label(
+    janela,
+    text="Sistema de Gestão de Estoque",
+    font=("Arial", 16)
+)
+
+titulo.pack(pady=20)
+
+janela.mainloop()
